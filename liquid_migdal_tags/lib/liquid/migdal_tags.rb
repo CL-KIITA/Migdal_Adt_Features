db1ef0db9d68056c5e5efbeaf36bb65675b47999
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-11-29 00:55:55 +0900

    man:initial gem liquid-migdal re

---
+++ liquid_migdal_tags/lib/liquid/migdal_tags.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "migdal_tags/version"
+require "liquid"
+
+module Liquid
+  module MigdalTags
+    // Todo: Language intent as block and inline, with cla style
+    class LanguageBlock < 
+    // Todo: dartpad intent
+    // Todo: toc block
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
+
---
+++ liquid_migdal_tags/lib/liquid/migdal_tags/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Liquid
+  module MigdalTags
+    VERSION = "0.1.0"
+  end
+end
